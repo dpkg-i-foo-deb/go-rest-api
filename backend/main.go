d06cec8e43e98d10614d05b74040cde2f68361bf
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-11 13:21:40 -0500

    Tidy up main.go ♻️

--- backend/main.go
+++ backend/main.go
@@ -11,20 +11,37 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func initQueries() {
 
+	log.Print("Initializing database queries")
+
+	database.InitLoginStatements()
+
+	log.Print("Database queries initialized!")
+}
+
+func initEnvironment() {
 	log.Print("Initializing and loading environment")
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Couldn't load .env file", err)
 	}
-	routes.InitRouter()
-	database.InitDatabase()
-	routes.InitIndexRoutes()
+}
 
+func startServer() {
 	var router = routes.GetRouter()
 
 	fmt.Print("Server is running on port" + os.Getenv("SERVER_PORT") + "\n")
 	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_PORT"), router))
+}
+
+func main() {
+
+	initEnvironment()
+	routes.InitRouter()
+	database.InitDatabase()
+	initQueries()
+	routes.InitIndexRoutes()
+	startServer()
 
 }
