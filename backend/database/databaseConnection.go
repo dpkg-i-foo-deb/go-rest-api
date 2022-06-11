58e613bd10f2a56c4c2688d572b1a811295d75ae
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-11 13:21:05 -0500

    remove useless GetDatabase function ⚰️

--- backend/database/databaseConnection.go
+++ backend/database/databaseConnection.go
@@ -8,18 +8,14 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var database *sql.DB
+var Database *sql.DB
 
 func InitDatabase() {
 	var err error
 	log.Print("Attempting to connect to database...")
-	database, err = sql.Open("postgres", os.Getenv("CONNECTION_STRING"))
+	Database, err = sql.Open("postgres", os.Getenv("CONNECTION_STRING"))
 	if err != nil {
 		log.Fatal("Couldn't connect to database", err)
 	}
 	log.Print("Database connection successful!")
-}
-
-func GetDatabase() *sql.DB {
-	return database
 }
