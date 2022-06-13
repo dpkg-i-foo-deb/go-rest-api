48cd2b1dfb6bacea2f229aa760eaef10a97ecf97
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-13 11:59:50 -0500

    Fix typo ✏️

--- backend/models/task.go
+++ backend/models/task.go
@@ -3,7 +3,7 @@ package models
 type task struct {
 	Code        int    `json:"code"`
 	Name        string `json:"name"`
-	Tile        string `json:"title"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
 	Subtask     int    `json:"subtask"`
 	User        string `json:"user"`
