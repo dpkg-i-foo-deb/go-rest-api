dbe167ccfce5ae09d10635e1b9799ab82ce0efdb
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-14 14:18:05 -0500

    Improve statement file structure 🏗️

--- backend/database/userStatements.go
+++ backend/database/userStatements.go
@@ -7,11 +7,21 @@ import (
 
 var err error
 var LoginStatement *sql.Stmt
+var SignUpStatement *sql.Stmt
 
 func InitLoginStatements() {
 	LoginStatement, err = Database.Prepare("SELECT u.email, u.password FROM \"user\" u WHERE u.email = $1")
 
 	if err != nil {
 		log.Fatal("Couldn't initialize login statements ", err)
+	}
+}
+
+func InitSignUpStatements() {
+	SignUpStatement, err = Database.Prepare(
+		"INSERT INTO \"user\" (email,\"password\", first_name, last_name) VALUES ($1,$2,$3,$4) RETURNING email")
+
+	if err != nil {
+		log.Fatal("Couldn't initialize sign up statements ", err)
 	}
 }
