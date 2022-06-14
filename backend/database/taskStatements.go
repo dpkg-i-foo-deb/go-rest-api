1d8dd056328f3cc4ac34d0aa47ed5bfe3c7b8780
Mateo Estrada Ramirez <mateoestradaramirez2001@gmail.com>
2022-06-14 15:33:07 -0500

    Modify task statements 🗃️

--- backend/database/taskStatements.go
+++ backend/database/taskStatements.go
@@ -5,12 +5,14 @@ import (
 	"log"
 )
 
-var createTaskStatement *sql.Stmt
+var CreateTaskStatement *sql.Stmt
 
 func InitTaskStatements() {
-	createTaskStatement, err = Database.Prepare(`INSERT INTO public.task 
+	CreateTaskStatement, err = Database.Prepare(`INSERT INTO public.task 
 		(title,description,"user",start_date,due_date,status)
-		VALUES ($1, $2, $3, $4, $5, $6)`)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING 
+		title, description , "user",start_date ,due_date ,status ,main_task ,code `)
 
 	if err != nil {
 		log.Fatal("Couldn't initialize task statements ", err)
