da693ac5034d3249d16940a007c8cd1175d5fb12
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-01 19:01:01 -0500

    Add get task service ✨

--- backend/routes/taskRoutes.go
+++ backend/routes/taskRoutes.go
@@ -7,8 +7,13 @@ import (
 
 func InitTaskRoutes() {
 	createTaskRoute()
+	getTaskRoute()
 }
 
 func createTaskRoute() {
 	AddHandle("/create-task", auth.ValidateAndContinue(services.CreateTaskService), "PUT")
+}
+
+func getTaskRoute() {
+	AddHandle("/task/{code}", auth.ValidateAndContinue(services.GetTaskService), "GET")
 }
--- backend/services/taskServices.go
+++ backend/services/taskServices.go
@@ -4,17 +4,21 @@ import (
 	"backend/auth"
 	"backend/database"
 	"backend/models"
+	"backend/models/utils"
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func CreateTaskService(writer http.ResponseWriter, request *http.Request, bodyBytes []byte) {
 
-	var task models.Task
-	var claims *auth.CustomClaims
+	var task *models.Task
+	//var claims *auth.CustomClaims
 	var tokenString string
 	//Use the incoming request body bites instead of the request which is already closed
 	decoder := json.NewDecoder(ioutil.NopCloser(bytes.NewBuffer(bodyBytes)))
@@ -27,29 +31,27 @@ func CreateTaskService(writer http.ResponseWriter, request *http.Request, bodyBy
 		return
 	}
 
-	//We retrieve the token string from the request cookie
-
 	tokenString, err = auth.GetCookieValue(request, "access-token")
 
 	if err != nil {
-		log.Print("Could not retrieve the token string from the cookie ", err)
+		log.Print("Could not retrieve the token string ", err)
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	//Now we retrieve the claims from the token
-
-	claims, err = auth.GetTokenClaims(tokenString)
+	//We retrieve the token string from the request cookie
+	task.User = new(string)
+	*task.User, err = auth.EmailFromToken(tokenString)
 
 	if err != nil {
-		log.Print("Could not retrieve the claims from the token ", err)
+		log.Print("Could not retrieve the user's identity ", err)
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	//Finally, we set the task email using the claims
 
-	task.User = &claims.Email
+	//task.User = &claims.Email
 
 	if err != nil {
 		log.Print("Could not retrieve the auth cookie", err)
@@ -74,5 +76,62 @@ func CreateTaskService(writer http.ResponseWriter, request *http.Request, bodyBy
 	json.NewEncoder(writer).Encode(task)
 
 	log.Print("New task created!")
+
+}
+
+func GetTaskService(writer http.ResponseWriter, request *http.Request, bodyByes []byte) {
+
+	taskCode, err := strconv.ParseInt(mux.Vars(request)["code"], 10, 64)
+
+	var tokenString string
+
+	var userEmail string
+
+	var task models.Task
+
+	var errorResponse utils.GenericResponse
+
+	if err != nil {
+		log.Print("The received code is not correct")
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenString, err = auth.GetCookieValue(request, "access-token")
+
+	if err != nil {
+		log.Print("Could not retrieve the access token")
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	userEmail, err = auth.EmailFromToken(tokenString)
+
+	if err != nil {
+		log.Print("The token does not contain the user email")
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = database.GetTaskStatement.QueryRow(
+		taskCode, userEmail,
+	).Scan(
+		&task.Title, &task.Description, &task.Code,
+		&task.MainTask, &task.User, &task.StartDate, &task.DueDate, &task.Status,
+	)
+
+	if err != nil {
+		log.Print("The queried task does not exist or the user has no access to it ", err)
+
+		errorResponse.Response = "The task does not exist or you have no access to it"
+
+		json.NewEncoder(writer).Encode(errorResponse)
+
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	writer.WriteHeader(http.StatusFound)
+	json.NewEncoder(writer).Encode(task)
 
 }
