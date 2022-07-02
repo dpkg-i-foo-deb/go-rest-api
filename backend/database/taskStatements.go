0993b50ca6b4a7bf67632aa554f22905369225cf
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-02 10:02:14 -0500

    Add a new statement 🗃️

--- backend/database/taskStatements.go
+++ backend/database/taskStatements.go
@@ -7,6 +7,7 @@ import (
 
 var CreateTaskStatement *sql.Stmt
 var GetTaskStatement *sql.Stmt
+var GetAllTasksStatement *sql.Stmt
 
 func InitTaskStatements() {
 	CreateTaskStatement, err = Database.Prepare(`INSERT INTO public.task 
@@ -21,4 +22,15 @@ func InitTaskStatements() {
 
 	GetTaskStatement, err = Database.Prepare(`SELECT title, description, code, main_task, "user", start_date, due_date, status
 											FROM public.task t WHERE t.code=$1 AND t.user =$2`)
+
+	if err != nil {
+		log.Fatal("Couldn't initialize task statements ", err)
+	}
+
+	GetAllTasksStatement, err = Database.Prepare(`SELECT title, description, code, main_task, "user", start_date, due_date, status
+													FROM public.task t WHERE t.user = $1`)
+
+	if err != nil {
+		log.Fatal("Couldn't initialize task statements ", err)
+	}
 }
